28841c4fd0134c7927cdfbefb20279192c140a43
Mahmud wali <waliradim@gmail.com>
2021-09-24 23:23:54 +0600

    Section Branch test

--- src/Components/APIcontext/Section.js
+++ src/Components/APIcontext/Section.js
@@ -4,7 +4,7 @@ import Content from './Content'
 export default function Section({ theme }) {
     return (
         <div>
-            <h1>This Section Component</h1>
+            <h1>This Section Component Radim Wali Mahmud</h1>
             <Content theme={theme} />
         </div>
     )
