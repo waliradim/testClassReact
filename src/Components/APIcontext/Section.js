24343afaaef55d62b5ec2dc913b4a09577e2c8b1
waliradim <waliradim@gmail.com>
2021-09-27 10:41:44 +0600

    Section rendering off

--- src/Components/APIcontext/Section.js
+++ src/Components/APIcontext/Section.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Content from './Content'
 
-export default function Section() {
-    return (
-        <div>
-            <h1>This Section Component Radim Wali Mahmud</h1>
-            <Content />
-        </div>
-    )
+export default class Section extends React.Component {
+    shouldComponentUpdate() {
+        return false
+    }
+    render() {
+        return (
+            <div>
+                <h1>This Section Component Radim Wali Mahmud</h1>
+                <Content />
+            </div>
+        )
+    }
 }
